15fa82e5cac73bacfb9ec149c3c42212334e1152
noma4i <noma4i@gmail.com>
2014-08-07 18:01:16 +1000

    fix current

--- SubRed.py
+++ SubRed.py
@@ -1,20 +1,52 @@
 import sublime, sublime_plugin, os, sys, imp
 
-__path__ = os.path.dirname(__file__)
-libs_path = os.path.join(__path__, 'libs')
+sys.path.append(os.path.join(os.path.dirname(__file__), "libs"))
 
-import redmine
+
+from redmine import Redmine
 
 class RedClient(object):
-  def __init__(self, window):
-    sublime.active_window().create_output_panel("scope")
-    result_page = window.new_file()
-    result_page.set(title ,"d")
-    print(dir(result_page))
+  def __init__(self, edit):
+    settings = self.settings()
+    self.__url  = settings.get('redmine_url')
+    self.__api_key  = settings.get('api_key')
+    self.__build()
 
-  def dump(obj):
-    for attr in dir(obj):
-      print (attr, getattr(obj, attr))
+  def __build(self):
+    redmine = Redmine(self.__url, key=self.__api_key)
+    # issue = red_mine.issues
+    issue = redmine.issue.get(22245)
+    print(dir(issue))
+    print(issue.description)
+    self.scratch(issue.description, title="Redmine Issue"+str(issue.id))
+
+  def settings(self):
+    return sublime.load_settings("RedClient.sublime-settings")
+
+  def get_window(self):
+    return sublime.active_window()
+
+  def scratch(self, output, title=False, position=None, **kwargs):
+    scratch_file = self.get_window().new_file()
+    if title:
+        scratch_file.set_name(title)
+    scratch_file.set_scratch(True)
+    scratch_file.insert(edit, 0, output)
+
+    scratch_file.set_read_only(True)
+
+    return scratch_file
+  def _output_to_view(self, output_file, output, clear=False, syntax="Packages/Diff/Diff.tmLanguage", **kwargs):
+    output_file.set_syntax_file(syntax)
+    edit = output_file.begin_edit()
+    if clear:
+        region = sublime.Region(0, self.output_view.size())
+        output_file.erase(edit, region)
+    output_file.insert(edit, 0, output)
+    output_file.end_edit(edit)
+
 class SubRedCommand(sublime_plugin.TextCommand):
-  def run(self, edit, text):
-    self.view.replace(edit, sublime.Region(0, self.view.size()), text)
+  def run(self, edit):
+    RedClient(self)
+
+    # self.view.replace(edit, sublime.Region(0, self.view.size()), text)
