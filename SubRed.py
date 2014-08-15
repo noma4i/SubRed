2a9ac0abd42c7cf9a7769e3506d40e45b94be7c0
noma4i <noma4i@gmail.com>
2014-08-15 12:24:33 +1000

    refactoring and new features

--- SubRed.py
+++ SubRed.py
@@ -1,30 +1,49 @@
-import sublime, sublime_plugin, os, sys, imp, re, webbrowser, platform
+import sublime
+import sublime_plugin
+import os
+import sys
+import imp
+import re
+import webbrowser
+import platform
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "libs"))
 
-
 from redmine import Redmine
 
-cached_issue_id = 0
+class SubRedmine:
+  def connect():
+    settings = sublime.load_settings("SubRed.sublime-settings")
+    url  = settings.get('redmine_url')
+    api_key  = settings.get('api_key')
 
+    return Redmine(url, key=api_key)
+
+# View Issue
 class SubRedCommand(sublime_plugin.WindowCommand):
   def run(self):
-    self.window.show_input_panel("Isssue ID #:", "", self.get_issue, None, None)
+    self.window.show_input_panel("Issue ID #:", "", self.get_issue, None, None)
 
   def get_issue(self,text):
     if self.window.active_view():
       self.window.active_view().run_command( 'redmine_fetcher', {'issue_id': text} )
 
+# Redmine: Refresh Issue
+class SubRedRefreshIssueCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    self.view.run_command( 'redmine_fetcher', {'issue_id': 'self'} )
+    sublime.status_message('Issue Refreshed!')
+
+# Redmine: List Queries
 class SubRedGetQueryCommand(sublime_plugin.TextCommand):
   def run(self,edit):
-    global project_id
     def on_done(i):
       if i > -1:
         query = query_ids[i]
         project_id = query_projects[i]
-        self.view.run_command( 'redmine_query_list', {'project_id': project_id, 'query_id': query} )
+        self.view.run_command( 'redmine_fetch_query', {'project_id': project_id, 'query_id': query} )
 
-    redmine = RedmineFetcherCommand.init_redmine(self)
+    redmine = SubRedmine.connect()
     queries = redmine.query.all()
     query_names = []
     query_ids = []
@@ -34,32 +53,49 @@ class SubRedGetQueryCommand(sublime_plugin.TextCommand):
       query_ids.append(query.id)
       if hasattr(query, 'project_id'):
         query_projects.append(query.project_id)
+
     self.view.window().show_quick_panel(query_names, on_done)
 
+# Redmine: Set Status
+class SubRedSetStatusCommand(sublime_plugin.TextCommand):
+  def run(self,edit):
+    redmine = SubRedmine.connect()
+    issue_id = self.view.name().replace('Redmine Issue #','')
 
-class RedmineQueryListCommand(sublime_plugin.TextCommand):
-  def run(self,edit,project_id,query_id):
-    redmine = RedmineFetcherCommand.init_redmine(self)
-    issues = redmine.issue.filter(project_id=project_id,query_id=query_id)
-    self.redmine_view(edit, issues, title="Redmine Query List")
+    statuses = redmine.issue_status.all()
+    statuses_names = []
+    statuses_ids = []
+    for status in statuses:
+      statuses_names.append(status.name)
+      statuses_ids.append(status.id)
+    def on_done(i):
+      if i > -1:
+        if issue_id != 0:
+          issue = redmine.issue.get(issue_id)
+          issue.status_id = statuses_ids[i]
+          issue.save()
+          sublime.status_message('Issue #%r now is %s' % (issue.id, statuses_names[i]))
+          self.view.run_command( 'redmine_fetcher', {'issue_id': issue_id} )
+    if "Issue #" in self.view.name():
+      self.view.window().show_quick_panel(statuses_names, on_done)
+    else:
+      sublime.message_dialog("Open Issue!")
 
-  def redmine_view(self, edit, issues, title=False, position=None, **kwargs):
-    syntax = 'Packages/Markdown/Markdown.tmLanguage'
-    redmine_view = self.view.window().new_file()
-    redmine_view.set_name(title)
-    redmine_view.set_scratch(True)
-    redmine_view.set_syntax_file(syntax)
+# Redmine: Assigned To
+class SubRedSetAssignedCommand(sublime_plugin.TextCommand):
+  def run(self,edit):
+    issue_id = self.view.name().replace('Redmine Issue #','')
+    redmine = SubRedmine.connect()
+    current_user = redmine.user.get('current')
 
-    content = 'Total: %s\n' % len(issues)
-    content += '-------------------------------------------\n'
-    for issue in issues:
-      content += '#%r\t\t%s\n' % (issue.id,issue.subject)
+    issue = redmine.issue.get(issue_id)
+    issue.assigned_to_id = current_user.id
+    issue.save()
 
-    redmine_view.insert(edit, 0, content)
-    redmine_view.set_read_only(True)
+    sublime.status_message('Issue #%r is assigned to you!' % issue.id)
+    self.view.run_command( 'redmine_fetcher', {'issue_id': issue.id} )
 
-    return redmine_view
-
+# #Redmine: Open in Browser
 class SubRedGoRedmineCommand(sublime_plugin.TextCommand):
   def run(self,edit):
     current_os = platform.system()
@@ -71,85 +107,57 @@ class SubRedGoRedmineCommand(sublime_plugin.TextCommand):
       wbrowser = webbrowser.get('windows-default')
     else:
       wbrowser = webbrowser.get('mozilla')
-
     if issue_id:
       url = settings.get('redmine_url')+"/issues/"+issue_id
       wbrowser.open_new(url)
     else:
       sublime.message_dialog("Open Issue!")
 
-class SubRedSetStatusCommand(sublime_plugin.TextCommand):
-  def run(self,edit):
-    global cached_statuses, cached_issue_id
-    redmine = RedmineFetcherCommand.init_redmine(self)
-    statuses = redmine.issue_status.all()
-    statuses_names = []
-    statuses_ids = []
-    for status in statuses:
-      statuses_names.append(status.name)
-      statuses_ids.append(status.id)
+########################## View Actions ##########################
+class RedmineFetchQueryCommand(sublime_plugin.TextCommand):
+  def run(self,edit,project_id,query_id):
+    redmine = SubRedmine.connect()
+    issues = redmine.issue.filter(project_id=project_id,query_id=query_id)
+    self.redmine_view(edit, issues, title="Redmine Query List")
 
-    def on_done(i):
-      if i > -1:
-        if cached_issue_id != 0:
-          issue = redmine.issue.get(cached_issue_id)
-          issue.status_id = statuses_ids[i]
-          issue.save()
-          sublime.status_message('Issue #%r now is %s' % (issue.id, statuses_names[i]))
-          self.view.run_command( 'redmine_fetcher', {'issue_id': cached_issue_id} )
+  def redmine_view(self, edit, issues, title):
+    r = self.view.window().new_file()
+    r.set_name(title)
+    r.set_scratch(True)
+    r.set_syntax_file('Packages/Markdown/Markdown.tmLanguage')
 
-    region = sublime.Region(51,52)
-    issue_line = self.view.window().active_view().line(region)
-    issue_line_content = self.view.window().active_view().substr(issue_line)
+    content = 'Total: %s\n' % len(issues)
+    content += '-------------------------------------------\n'
+    for issue in issues:
+      content += '#%r\t\t%s\n' % (issue.id,issue.subject)
 
-    if "#Issue" in issue_line_content:
-      cached_issue_id = issue_line_content.replace('#Issue ','')
-      self.view.window().show_quick_panel(statuses_names, on_done)
-    else:
-      sublime.message_dialog("Open Issue!")
-
+    r.insert(edit, 0, content)
+    r.set_read_only(True)
 
 class RedmineFetcherCommand(sublime_plugin.TextCommand):
   def run(self, edit, issue_id):
-    global cached_issue_id
+    redmine = SubRedmine.connect()
+    if issue_id == 'self':
+      issue_id = self.view.name().replace('Redmine Issue #','')
+    try:
+      issue = redmine.issue.get(issue_id)
+    except:
+      sublime.status_message('Issue #%s not found!' % (issue_id))
 
-    redmine = self.init_redmine()
-    issue = redmine.issue.get(issue_id)
     self.redmine_view(edit, issue, title="Redmine Issue #"+str(issue.id))
-    cached_issue_id = issue.id
 
-  def init_redmine(self):
-    settings = sublime.load_settings("SubRed.sublime-settings")
-    url  = settings.get('redmine_url')
-    api_key  = settings.get('api_key')
-
-    return Redmine(url, key=api_key)
-
-  def get_window(self):
-    return sublime.active_window()
-
-  def redmine_view(self, edit, issue, title=False, position=None, **kwargs):
-    syntax = 'Packages/Markdown/Markdown.tmLanguage'
+  def redmine_view(self, edit, issue, title):
     if self.view.name() == ("Redmine Issue #"+str(issue.id)):
-      redmine_view = self.view
-      redmine_view.set_read_only(False)
-      redmine_view.erase(edit, sublime.Region(0, self.view.size()))
+      r = self.view
+      r.set_read_only(False)
+      r.erase(edit, sublime.Region(0, self.view.size()))
     else:
-      redmine_view = self.get_window().new_file()
-      redmine_view.set_name(title)
-      redmine_view.set_scratch(True)
-      redmine_view.set_syntax_file(syntax)
+      r = sublime.active_window().new_file()
+      r.set_name(title)
+      r.set_scratch(True)
+      r.set_syntax_file('Packages/Markdown/Markdown.tmLanguage')
 
-    desc = issue.description.replace("\r", "")
-
-
-    # -------------------------------------------
-    # #Issue #22089
-    # ###### Status:         QA Rejected
-    # ###### Priority:       Low
-    # ###### Assigned to:    Alex
-
-    # -------------------------------------------
+    desc = issue.description.replace("\r", "").replace("\n","\n#|\t\t")
 
     content = '-------------------------------------------\n'
     content += '#Issue %s\n' % issue.id
@@ -160,16 +168,21 @@ class RedmineFetcherCommand(sublime_plugin.TextCommand):
     content += '# %s\n' % issue.subject
     content += '-------------------------------------------\n\n\n'
 
+    if len(issue.attachments) > 0:
+      content += '---'
+    for f in issue.attachments:
+      content += '\n#\t%r (%s)' % (f.filename,f.content_url)
+    if len(issue.attachments) > 0:
+      content += '\n---\n\n'
+
     content += '[%r](%s)' % (issue.created_on.strftime("%A %d. %B %Y"), issue.author.name)
-    content += '\n#|\t\t%s\n\n\n' % desc.replace("\n","\n#|\t\t")
+    content += '\n#|\t\t%s\n\n\n' % desc
     for journal in issue.journals:
       if hasattr(journal, 'notes'):
         if journal.notes != '':
           content += '[%r](%s)\n' % (journal.created_on.strftime('%A %d. %B %Y'), journal.user)
           content += '\t%s\n' % journal.notes.replace('\r', '')
           content += '\n'
-    redmine_view.insert(edit, 0, content)
 
-    redmine_view.set_read_only(True)
-
-    return redmine_view
+    r.insert(edit, 0, content)
+    r.set_read_only(False)
